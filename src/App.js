2bc899497cd004ed79600458d04e6bdd3fecc148
Abul22 <luke.luklus@gmail.com>
2019-05-26 21:30:17 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <script> alert('test') </script>
           <a
             className="App-link"
             href="https://reactjs.org"
